9ab83963074993bfdbbb42b4f22522520829cd5f
sm100499078 <100499078@alumnos.uc3m.es>
2025-10-23 17:32:05 +0200

    Commit juegos

--- js/script.js
+++ js/script.js
@@ -140,3 +140,42 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+// ======================================
+
+// Home: cookies
+
+function setCookie(cname, cvalue, exdays){
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    let expires = "expires=" +d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + "path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name)==0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+// No terminado
+function checkCookie() {
+  let username = getCookkie("login-email");
+  if (username != ""){
+    alert("Hola de nuevo" + username);
+  } else {
+    username = prompt("Por favor introduzca su nombre:", "");
+    if(username != "" && username != null){
+      setCookie("login-email", username, 365);
+    }
+  }
+}
