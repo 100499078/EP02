c966bda96f1a034763b9295c88f06ec2401e614c
sm100499078 <100499078@alumnos.uc3m.es>
2025-10-24 15:54:34 +0200

    commit

---
+++ js/login.js
@@ -0,0 +1,31 @@
+// ----------------- INICIO DE SESIÓN -----------------
+
+document.addEventListener("DOMContentLoaded", function() {
+  const form = document.getElementById("form-login");
+  const mensaje = document.getElementById("mensaje-login");
+  const emailInput = document.getElementById("login-email");
+  const passwordInput = document.getElementById("login-pass");
+
+  if(form){
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+
+      // Obtenemos el usuario del localStorage
+      const usuarioGuardado = JSON.parse(localStorage.getItem("usuarioActual"));
+      if (!usuarioGuardado) {
+        mostrarMensaje("No hay usario registrado con ese mail. Por favor, regístrese.", false);
+        return;
+      }
+      const email = emailInput.value.trim();
+      const password = passwordInput.value.trim();
+
+      // Validamos las credenciales
+      if (emailInput.value.trim() === usuarioGuardado.email && passwordInput.value === usuarioGuardado.password) {
+        window.location.href = "indexB.html";
+      }
+      else {
+        mostrarMensaje("Email o contraseña incorrectos.", false);
+      }
+    });
+  }
+});
