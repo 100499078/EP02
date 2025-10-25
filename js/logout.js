d2a6f86f712f1b6a98237ec2a0145ab9e547dd2a
sm100499078 <100499078@alumnos.uc3m.es>
2025-10-25 14:00:05 +0200

    commit

--- js/logout.js
+++ js/logout.js
@@ -1,30 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const btnLogout = document.querySelector(".sidebar.logout");
-    const mode = document.getElementById("mode-logout");
-    const confirmBtn = document.getElementById("btn-confirmLogout");
-    const cancelBtn = document.getElementById("btn-cancelLogout");
 
-    // Mostrar el modo de confirmación al hacer clic en el botón de cerrar sesión
-    btnLogout.addEventListener("click", () => {
-        mode.style.display = "flex"; // Centrar el contenido
-    });
+const btnLogout = document.querySelector(".sidebar-logout");
+const mode = document.getElementById("mode-logout");
+const confirmBtn = document.getElementById("btn-confirmLogout");
+const cancelBtn = document.getElementById("btn-cancelLogout");
 
-    // Cerrar el modo al hacer click en Cancelar
-    cancelBtn.addEventListener("click", () => {
+// Mostrar el modo de confirmación al hacer clic en el botón de cerrar sesión
+btnLogout.addEventListener("click", () => {
+    mode.style.display = "flex"; // Centrar el contenido
+});
+
+// Cerrar el modo al hacer click en Cancelar
+cancelBtn.addEventListener("click", () => {
+    mode.style.display = "none";
+});
+
+// Confirmar cierre de sesión
+confirmBtn.addEventListener("click", () => {
+    sessionStorage.clear();
+    localStorage.clear();
+
+    window.location.href = "index.html";
+});
+
+window.addEventListener("click", (e) => {
+    if (e.target === mode) {
         mode.style.display = "none";
-    });
-
-    // Confirmar cierre de sesión
-    confirmBtn.addEventListener("click", () => {
-        sessionStorage.clear();
-        localStorage.clear();
-
-        window.location.href = "index.html";
-    });
-
-    window.addEventListener("click", (e) => {
-        if (e.target === mode) {
-            mode.style.display = "none";
-        }
-    });
+    }
 });
